568e25d214a39ad41e5fd2b6bb5c820fc1469de4
gomitaa. <gomitasdetiburon21@gmail.com>
2025-02-01 15:12:43 -0300

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,9 +1,15 @@
 ESX = nil
+
+-- Obtener ESX
 ESX = exports['es_extended']:getSharedObject()
 
-RegisterNetEvent('pedidosYa:pagar')
-AddEventHandler('pedidosYa:pagar', function(monto)
-    local _source = source
-    local xPlayer = ESX.GetPlayerFromId(_source)
-    xPlayer.addMoney(monto)  -- Paga el monto al jugador
+RegisterNetEvent('pedidosYa:comenzarEntrega')
+AddEventHandler('pedidosYa:comenzarEntrega', function()
+    local pagoNormal = math.random(60000, 100000)  -- Pago entre 60,000 y 100,000
+
+    -- Pago completo
+    TriggerClientEvent('esx:showNotification', source, '¡Pedido entregado! Recibiste el pago completo de $' .. pagoNormal)
+
+    -- Pagar al jugador el monto completo
+    TriggerServerEvent('pedidosYa:pagar', pagoNormal)
 end)
